e82b6ed9cd9258016f37830345318edbaf4940a3
mateusborges20 <borgesmateus099@gmail.com>
2025-03-22 17:28:20 -0300

    email funcionando

--- accordion.js
+++ accordion.js
@@ -21,41 +21,37 @@ menuLinks.forEach((link) => {
   });
 });
 
-
 //accordion -list
-const accordionList = document.querySelectorAll('.accordion-list dt');
-accordionList[0].classList.add('ativo');
-accordionList[0].nextElementSibling.classList.add('ativo');
-function  activeAcordion() {
-  this.classList.toggle('ativo');
-  this.nextElementSibling.classList.toggle('ativo');
+const accordionList = document.querySelectorAll(".accordion-list dt");
+accordionList[0].classList.add("ativo");
+accordionList[0].nextElementSibling.classList.add("ativo");
+function activeAcordion() {
+  this.classList.toggle("ativo");
+  this.nextElementSibling.classList.toggle("ativo");
 }
 
 accordionList.forEach((item) => {
-  item.addEventListener('click', activeAcordion);
-})
-
+  item.addEventListener("click", activeAcordion);
+});
 
 //scrool suave.
 
 const linksInternos = document.querySelectorAll('a[href^= "#"');
 console.log(linksInternos);
 
-
 function scrollToSection(event) {
   event.preventDefault();
-  const href = event.currentTarget.getAttribute('href');
-  
+  const href = event.currentTarget.getAttribute("href");
+
   const section = document.querySelector(href);
-  
+
   section.scrollIntoView({
-    behavior: 'smooth',
-    block: 'start',
-  })
-  
+    behavior: "smooth",
+    block: "start",
+  });
 }
 
-
 linksInternos.forEach((item) => {
-  item.addEventListener('click', scrollToSection)
-})
+  item.addEventListener("click", scrollToSection);
+});
+
