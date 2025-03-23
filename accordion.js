197342fae3a31524bede511e0daf16c418aeb26d
mateusborges20 <borgesmateus099@gmail.com>
2025-03-23 14:16:02 -0300

    ajustes no scrool suave animação

--- accordion.js
+++ accordion.js
@@ -134,7 +134,7 @@ document
   .addEventListener("submit", enviarFormulario);
 
 const sections = document.querySelectorAll("section");
-const windowMetade = window.innerHeight * 0.6;
+const windowMetade = window.innerHeight * 0.8;
 
 function animaScroll() {
   sections.forEach((section, index) => {
